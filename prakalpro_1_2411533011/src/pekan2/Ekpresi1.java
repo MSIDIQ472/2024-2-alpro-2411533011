a6b1ecc9bff342585049441814e5e6329432b1ea
HP <HP@FTI-LAB03-PC09>
2024-09-26 11:01:27 +0700

    Prakalpro pekan 1 dan 2

---
+++ prakalpro_1_2411533011/src/pekan2/Ekpresi1.java
@@ -0,0 +1,24 @@
+package pekan2;
+
+public class Ekpresi1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("3 x 4 = ");
+	System.out.println(3*4);
+	System.out.println(14/4);
+	System.out.println(12%4);
+	System.out.println(32/5);
+	System.out.println(32%5);
+	System.out.println(84/10);
+	System.out.println(8421%10);
+	System.out.println(8421%1000);
+	System.out.println(1-2-3);
+	System.out.println(1+3*4);
+	System.out.println((1+3)*4);
+	System.out.println(2/3);
+}
+	}
+
+
+
